e28ab6f0a8bfd061560df9258d4fecc2020897cd
Timofey Bystrov <timapro21@gmail.com>
2019-12-08 19:51:13 +0300

    fix filters request model

--- src/main/kotlin/kek/foundation/backend/filters/Filter.kt
+++ src/main/kotlin/kek/foundation/backend/filters/Filter.kt
@@ -2,12 +2,11 @@ package kek.foundation.backend.filters
 
 private const val MIN_YEAR = 1974
 private const val MAX_YEAR = 1996
-private const val NOT_EXTENDED = false
 
 data class Filter(
     val minYear: Int = MIN_YEAR,
     val maxYear: Int = MAX_YEAR,
-    val isExtended: Boolean = NOT_EXTENDED,
+    val isExtended: Boolean?,
     val countries: List<Int>?,
     val regions: List<Int>?,
     val isSuccess: Boolean?,
